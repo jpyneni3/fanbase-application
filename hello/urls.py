81e209d09be254a18ed7db82a979c188db05b938
tomonarifeehan <feehan.tomonari@gmail.com>
2017-07-14 09:21:27 -0400

    Added db to urlpatterns

--- hello/urls.py
+++ hello/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^signup/$', views.signup, name='signup'),
 	url(r'^login/$', views.login, name='login'),
-	url(r'^festival/$', views.festival, name='festival')
+	url(r'^festival/$', views.festival, name='festival'),
+	url(r'^db', hello.views.db, name='db')
 ] 
